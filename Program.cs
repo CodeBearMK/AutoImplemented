d24ca43e2b3babd5779e78f1b801252e476978e1
cat32tw <mike1993xd@gmail.com>
2022-02-13 16:05:09 +0800

    自動實作練習

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AutoImplemented
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Length = 10; //可以透過set寫入值
+            rectangle.SetLength1(100); //只能透過方法改寫
+            Console.WriteLine(rectangle.Length);
+            Console.WriteLine(rectangle.Length1);
+            Console.Read();
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoImplemented
+{
+    internal class Rectangle
+    {
+        public int Length { get; set; }
+        public int Wide { get; set; }
+
+        //唯讀屬性
+        public int Length1 { get; private set; }
+        public int Wide1 { get; private set; }
+
+        //或者
+        public int Length2 { get; }
+        public int Wide2 { get; }
+
+        public void SetLength1(int tlength)
+        {
+            Length1 = tlength;
+        }
+    }
+}
